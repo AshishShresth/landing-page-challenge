9e426702abab2cb1027e3d85a062eba170846e82
Ashish Shrestha <ashish11stha@gmail.com>
2022-03-06 11:47:54 +0545

    updated README file

--- script.js
+++ script.js
@@ -1,8 +1,16 @@
 const navToggle = document.querySelector('.nav-toggle');
+let navOpen = false;
 const nav = document.querySelector('.nav');
 const header = document.querySelector('.header');
 
 navToggle.addEventListener('click', () => {
   nav.classList.toggle('nav--visible');
   header.classList.toggle('header--dark');
+  if (!navOpen) {
+    navToggle.classList.add('open');
+    navOpen = true;
+  } else {
+    navToggle.classList.remove('open');
+    navOpen = false;
+  }
 });
